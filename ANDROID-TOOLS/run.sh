f864da30c5c6640ba40153600b91f3ba770811a5
Your Name <you@example.com>
2022-08-12 19:25:20 +0530

    tools

--- ANDROID-TOOLS/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo -S <<< "p" dpkg -i ./brotli*.deb -y
-sudo -S <<< "p" mkdir /opt/bin
-sudo -S <<< "p" mv sdat2img.py  /opt/bin
-echo "export PATH=/opt/bin:$PATH" >> ~/.profile
-source ~/.profile
-exit 
-
